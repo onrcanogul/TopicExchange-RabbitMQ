960684a9f8d4f8b17f21c961aaf6f1f268563bb6
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-04-25 15:39:35 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://yejguzib:NuvHgMGxrkfxK8SHh3VRyAYLi3B3ENf1@shark.rmq.cloudamqp.com/yejguzib");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+Console.Write("Topic(Listener): ");
+string topic = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: topic);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+};
+
+Console.Read();
+
+
+
---
+++ Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://yejguzib:NuvHgMGxrkfxK8SHh3VRyAYLi3B3ENf1@shark.rmq.cloudamqp.com/yejguzib");
+
+
+using IConnection connection =  factory.CreateConnection();
+using IModel channel =  connection.CreateModel();
+
+channel.ExchangeDeclare("topic-exchange-example", type: ExchangeType.Topic);
+
+while(true)
+{
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+    Console.Write("Topic(Sender): ");
+    string topic = Console.ReadLine();
+
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: byteMessage
+        );
+
+}
+
+Console.Read();
